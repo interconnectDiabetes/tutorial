3c7a5c650f7a03356ef905475ebcf45d0813ef6c
Paul Scherer <paul.scherer@mrc-epid.cam.ac.uk>
2016-10-20 15:18:18 +0100

    completed some skeleton code to get people started

--- advanced_task.R
+++ advanced_task.R
@@ -73,18 +73,6 @@ rownames(summary_sex) <- study_names
 colnames(summary_sex) <- c("type", "N", "male", "female", "count0", "count1")
 rm(summary_sex_temp)
 
-# gestational age by sex - most of the code is to sort out formatting
-summary_ga_temp <- ds.meanByClass(x='E4$GESTATIONAL_AGE~E4$SEX', type = 'split')
-summary_ga <- matrix(unlist(summary_ga_temp))
-summary_ga <- substr(summary_ga, 1, nchar(summary_ga)-1)
-summary_ga <- summary_ga[seq(2, nrow(summary_ga), by = 2),]
-summary_ga <- do.call('rbind', strsplit(as.character(summary_ga),'(',fixed=TRUE))
-summary_ga <- cbind(summary_ga[seq(1, nrow(summary_ga), by = 2),],summary_ga[seq(2, nrow(summary_ga), by = 2),])
-summary_ga <- data.frame(t(apply(summary_ga, 1, as.numeric)))
-rownames(summary_ga) <- study_names
-colnames(summary_ga) <- c("mean_0", "sd_0", "mean_1", "sd_1")
-rm(summary_ga_temp)
-
 #mothers'characteristics
 #BMI
 mean_bmi <- data.frame(matrix(unlist(ds.mean("E4$MATERNAL_BMI", type = 'split')), nrow = num_studies, ncol = 1, byrow=TRUE))
@@ -94,97 +82,8 @@ rownames(summary_bmi) <- study_names
 colnames(summary_bmi) <- c("mean", "sd")
 rm(mean_bmi, var_bmi)
 
-
-#Age
-mean_age <- data.frame(matrix(unlist(ds.mean("E4$MATERNAL_AGE", type = 'split')), nrow = num_studies, ncol = 1, byrow=TRUE))
-var_age <- data.frame(matrix(unlist(datashield.aggregate(opals, as.symbol("varDS(E4$MATERNAL_AGE)"))), nrow = num_studies, ncol = 1, byrow=TRUE))
-summary_age <- cbind(mean_age,sqrt(var_age))
-rownames(summary_age) <- study_names
-colnames(summary_age) <- c("mean", "sd")
-rm(mean_age, var_age)
-
-
-# binaries
-binary_var <- c('SMOKING', 'GDM')
-binary_df <- data.frame()
-for (bin in binary_var) {
-  summary_temp <- ds.summary(paste0('E4$',bin))
-  summary_temp <- data.frame(matrix(unlist(summary_temp), nrow = num_studies, ncol=6, byrow=TRUE))
-  rownames(summary_temp) <- paste0(study_names,'_',bin)
-  binary_df <- rbind(binary_df, summary_temp)
-}
-colnames(binary_df) <- c('type', 'n', '0', '1', 'No', 'Yes')
-binary_df <- binary_df[,c(5,6)]
-rm(summary_temp)
-
-#maternal obesity
-summary_ob_temp <- ds.summary('E4$MATERNAL_OB')
-summary_ob <- data.frame(matrix(unlist(summary_ob_temp), nrow = num_studies, ncol=8, byrow=TRUE))
-rownames(summary_ob) <- study_names
-colnames(summary_ob) <- c("type", "N", "0", "1", "2", "Normal", "Overweight", "Obese")
-summary_ob <- summary_ob[,c(6,7,8)]
-rm(summary_ob_temp)
-
-#---------------------------------------------------------
-# Summaries for exposures
-
-#LTPA
-summary_ltpa_temp <- ds.summary('E4$LTPA_DUR_3')
-summary_ltpa <- data.frame(matrix(unlist(summary_ltpa_temp), nrow = num_studies, ncol=10, byrow=TRUE))
-rownames(summary_ltpa) <- study_names
-colnames(summary_ltpa) <- c("type", "N", "5%", "10%", "25%", "50%", "75%", "90%", "95%", "mean")
-summary_ltpa <- summary_ltpa[,c(2,6,5,7)]
-rm(summary_ltpa_temp)
-
-# MOD_VIG_3
-summary_mod_temp <- ds.summary('E4$MOD_VIG_3')
-summary_mod <- data.frame(matrix(unlist(summary_mod_temp), nrow = num_studies, ncol=10, byrow=TRUE))
-rownames(summary_mod) <- study_names
-colnames(summary_mod) <- c("type", "N", "5%", "10%", "25%", "50%", "75%", "90%", "95%", "mean")
-summary_mod <- summary_mod[,c(2,6,5,7)]
-rm(summary_mod_temp)
-
-# LTPA_EE_3
-summary_ee_temp <- ds.summary(x='E4$LTPA_EE_3')
-summary_ee <- data.frame(matrix(unlist(summary_ee_temp), nrow = num_studies, ncol=10, byrow=TRUE))
-rownames(summary_ee) <- study_names
-colnames(summary_ee) <- c("type", "N", "5%", "10%", "25%", "50%", "75%", "90%", "95%", "mean")
-summary_ee <- summary_ee[,c(2,6,5,7)]
-rm(summary_ee_temp)
-
-#---------------------------------------------------------
-# Summaries for outcomes
-
-# Birthweight by sex
-summary_bw_temp <- ds.meanByClass(x='E4$BIRTH_WEIGHT~E4$SEX', type = 'split')
-summary_bw <- matrix(unlist(summary_bw_temp))
-summary_bw <- substr(summary_bw, 1, nchar(summary_bw)-1)
-summary_bw <- summary_bw[seq(2, nrow(summary_bw), by = 2),]
-summary_bw <- do.call('rbind', strsplit(as.character(summary_bw),'(',fixed=TRUE))
-summary_bw <- cbind(summary_bw[seq(1, nrow(summary_bw), by = 2),],summary_bw[seq(2, nrow(summary_bw), by = 2),])
-summary_bw <- data.frame(t(apply(summary_bw, 1, as.numeric)))
-rownames(summary_bw) <- study_names
-colnames(summary_bw) <- c("mean_male", "sd_male", "mean_female", "sd_female")
-rm(summary_bw_temp)
-
-# Macrosomia
-summary_mac_temp <- ds.summary('E4$MACROSOMIA')
-summary_mac <- data.frame(matrix(unlist(summary_mac_temp), nrow = num_studies, ncol=6, byrow=TRUE))
-rownames(summary_mac) <- study_names
-summary_mac <- summary_mac[,c(1,2,5,6)]
-colnames(summary_mac) <- c("class", "length", "No", "Yes")
-rm(summary_mac_temp)
-
-# BIRTHWEIGHT_LGA
-summary_lga_temp <- ds.summary('E4$BIRTH_WEIGHT_LGA')
-summary_lga <- data.frame(matrix(unlist(summary_lga_temp), nrow = num_studies, ncol=6, byrow=TRUE))
-rownames(summary_lga) <- study_names
-summary_lga <- summary_lga[,c(1,2,5,6)]
-colnames(summary_lga) <- c("class", "length", "No", "Yes")
-rm(summary_lga_temp)
-
-
-#--------------- FUNCTIONS TO HELP WITH REGRESSIONS AND REM ------------------#
+# if you can figure out how this works its really nice helper for regression, how this works wont be immediately clear
+# so we press on
 do_reg <- function(my_fmla, study, outcome, out_family){
   
   model <- ds.glm(formula= my_fmla, data = ref_table, family = out_family, datasources=opals[i], maxit = 100)
@@ -197,34 +96,46 @@ do_reg <- function(my_fmla, study, outcome, out_family){
   return(model_coeffs)
 }
 
-do_REM <- function(coeffs, s_err, labels, fmla, out_family, variable){
-  
-  res <- rma(yi = coeffs, sei = s_err, method='DL', slab = labels)
-  
-  #forest plots
-  
-  if (out_family == 'gaussian') {
-    usr <- par("usr")
-    forest(res, mlab=bquote(paste('Overall (I'^2*' = ', .(round(res$I2)),'%, p = ',
-                                  .(round(res$QEp,3)),')')),
-           xlab=bquote(paste('Test of H'[0]*': true mean association = 0, p = ',
-                             .(round(res$pval,3)))), ilab = cbind(weights.rma.uni(res)),ilab.xpos = c(usr[1]))
-    text(usr[2], usr[4], "Beta [95% CI]", adj = c(1, 8))
-    #text(usr[1], usr[4], gsub(paste0(ref_table,"\\$"),"", Reduce(paste, deparse(fmla))), adj = c( 0, 8 ))
-    text(usr[1], usr[4], paste0(gsub(paste0(ref_table,"\\$"),"", deparse(fmla)),collapse="\n"), adj = c( 0, 1 ))
-    text(usr[1], usr[3], variable, adj = c( 0, 0 ))
-  }
-  else if (out_family == 'binomial'){
-    usr <- par("usr")
-    forest(res, digits=3, mlab=bquote(paste('Overall (I'^2*' = ', .(round(res$I2)),'%, p = ',
-                                            .(round(res$QEp,3)),')')),
-           xlab=bquote(paste('Test of H'[0]*': true relative risk = 1, p = ',
-                             .(round(res$pval,3)))),ilab = cbind(weights.rma.uni(res)),ilab.xpos = c(usr[1]), atransf = exp)
-    text(usr[2], usr[4], "Relative Risk [95% CI]", adj = c(1, 8))
-    #text(usr[1], usr[4], gsub(paste0(ref_table,"\\$"),"", Reduce(paste, deparse(fmla))), adj = c( 0, 8 ))
-    text(usr[1], usr[4], paste0(gsub(paste0(ref_table,"\\$"),"", deparse(fmla)),collapse="\n"), adj = c( 0, 1 ))
-    text(usr[1], usr[3], variable, adj = c( 0, 0))
-  }
-  
-  return(res)
+
+#  /'\_/`\            /\ \        /\_ \       /' \
+# /\      \    ___    \_\ \     __\//\ \     /\_, \
+# \ \ \__\ \  / __`\  /'_` \  /'__`\\ \ \    \/_/\ \
+#  \ \ \_/\ \/\ \L\ \/\ \L\ \/\  __/ \_\ \_     \ \ \
+#   \ \_\\ \_\ \____/\ \___,_\ \____\/\____\     \ \_\
+#    \/_/ \/_/\/___/  \/__,_ /\/____/\/____/      \/_/
+
+my_exposure = c('MOD_VIG_3', 'LTPA_DUR_3', 'LTPA_EE_3')
+my_outcome = c( 'BIRTH_WEIGHT','MACROSOMIA','BIRTH_WEIGHT_LGA')
+my_covariate = c('GESTATIONAL_AGE', 'SEX')
+
+ref_table = 'E4'
+
+# normal linear regressions made of formulas, data to act on, an error distribution function
+# linear regressions in Data shield not much different for the user they are also are made of formulas, data to act on, 
+# an error distribution function, plus the machine it came from 
+# fitted_model <- ds.glm(formula= my_fmla, data = ref_table, family = out_family, datasources=opals[i], maxit = 100)
+# the maxit just tells the maximum number of iterations we allow the fitting process to repeat itself.
+
+# lets make sure that our linear regression is done on the right family type, ie categorical values will need a binomial distribution, etc
+out_class = ds.class('E4$SOMEOUTCOMEGOODNESS')[[1]]
+if (out_class == 'factor'){
+	outcome_family = 'binomial'
+} else if (out_class == 'numeric' | out_class == 'integer') {
+	outcome_family = 'gaussian'
 }
+
+
+
+#  /'\_/`\            /\ \        /\_ \        /'___`\
+# /\      \    ___    \_\ \     __\//\ \      /\_\ /\ \
+# \ \ \__\ \  / __`\  /'_` \  /'__`\\ \ \     \/_/// /__
+#  \ \ \_/\ \/\ \L\ \/\ \L\ \/\  __/ \_\ \_      // /_\ \
+#   \ \_\\ \_\ \____/\ \___,_\ \____\/\____\    /\______/
+#    \/_/ \/_/\/___/  \/__,_ /\/____/\/____/    \/_____/
+
+my_exposure = c('MOD_VIG_3', 'LTPA_DUR_3','LTPA_EE_3')
+my_outcome = c('BIRTH_WEIGHT','MACROSOMIA','BIRTH_WEIGHT_LGA')
+my_covariate = c('GESTATIONAL_AGE', 'SEX', 'PARITY', 'MATERNAL_AGE', 'SMOKING',
+                 'ALCOHOL', 'MATERNAL_EDU', 'ETHNICITY')
+
+ref_table = 'E4'
